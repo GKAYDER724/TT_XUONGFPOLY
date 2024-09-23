9152d23140d14045f84308a7789558b7a04d64a6
GKAYDER724 <145838116+GKAYDER724@users.noreply.github.com>
2024-09-23 16:40:35 +0700

    thêm giao diện

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
 import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Home from './components/Home';
 import Header from './components/Header';
 import Footer from './components/Footer';
 import TicketPage from './pages/TicketPage';
@@ -7,16 +9,19 @@ import { LanguageProvider } from './context/LanguageContext';
 
 function App() {
   return (
+    <Router>
     <div className="d-flex flex-column min-vh-100">
       <Header />
-      <main className="flex-grow-1">
-        <TicketPage />
-      </main>
+      <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/ticket" element={<TicketPage />} />
+        </Routes>
       <LanguageProvider>
         <Footer />
       </LanguageProvider>
       <ChatBox />
     </div>
+    </Router>
   );
 }
 
