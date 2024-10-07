eeb0e9c44a4cb8180c175e3e025744ee3e1c9de7
GKAYDER724 <145838116+GKAYDER724@users.noreply.github.com>
2024-10-07 15:18:58 +0700

    giao diện

--- src/App.js
+++ src/App.js
@@ -1,20 +1,44 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Home from './pages/Home';
 import Header from './components/Header';
 import Footer from './components/Footer';
+import Login from './pages/login';
+import Register from './pages/register';
+import Hosting from './pages/Hosting';
+import Domain from './pages/domain';
+import Quickguide from './pages/quickguide';
+import Transfer from './pages/transfer';
+import Quick1 from './pages/quick1';
+import Quick2 from './pages/quick2';
+import Quick3 from './pages/quick3';
+import Transfer1 from './pages/transfer1';
+import Transfer2 from './pages/transfer2';
 import TicketPage from './pages/TicketPage';
 import ServiceDetail from './components/ServiceDetail';
 import ChatBox from './components/ChatBox';
-
+import "./assets/style.css";
 function App() {
   return (
-    <div className="d-flex flex-column min-vh-100">
+    <Router>
       <Header />
-      <main className="flex-grow-1">
-        <TicketPage />
-      </main>
-      <Footer />
-      <ChatBox />
-    </div>
+        <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/register" element={<Register />} />
+            <Route path="/domain" element={<Domain />} />
+            <Route path="/hosting" element={<Hosting />} />
+            <Route path="/quickguide" element={<Quickguide />} />
+            <Route path="/transfer" element={<Transfer />} />
+            <Route path="/quick1" element={<Quick1 />} />
+            <Route path="/quick2" element={<Quick2 />} />
+            <Route path="/quick3" element={<Quick3 />} />
+            <Route path="/transfer1" element={<Transfer1 />} />
+            <Route path="/transfer2" element={<Transfer2 />} />
+            <Route path="/ticket" element={<TicketPage />} />
+          </Routes>
+        <Footer />
+    </Router>
   );
 }
 
